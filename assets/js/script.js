30126053145986619126c391da19ca50b5498a95
KarlPreisler <Karlpreisler@gmail.com>
2022-11-04 11:16:38 +0000

    Listen for Clicks and assign them to playerChoice in JavaScript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,11 @@
 const playerResult = document.getElementById("player-choice")
 const computerResult = document.getElementById("computers-choice")
 const allChoices = document.querySelectorAll('button')
+
+/* event listener for playerChoice */
+
+allChoices.forEach(allChoices => allChoices.addEventListener('click', (e) => {
+    playerChoice = e.target.id
+    playerResult.innerHTML = playerChoice
+    playerImage.src = `assets/images/${[playerChoice]}.png.png`;
+}))

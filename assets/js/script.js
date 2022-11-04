c13c3818d42bae7d0e43b09ad3e0f35186e7812f
KarlPreisler <Karlpreisler@gmail.com>
2022-11-04 11:13:05 +0000

    add gamearea,player and computerchoice classes. declare variables in JS

---
+++ assets/js/script.js
@@ -0,0 +1,3 @@
+const playerResult = document.getElementById("player-choice")
+const computerResult = document.getElementById("computers-choice")
+const allChoices = document.querySelectorAll('button')

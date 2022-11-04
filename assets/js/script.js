3c582af0debd9cdc73f6f5567668e9f8a3c674a4
KarlPreisler <Karlpreisler@gmail.com>
2022-11-04 11:33:04 +0000

    Add logic for the  game to generate results per match

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,8 @@ const playerResult = document.getElementById("player-choice")
 const computerResult = document.getElementById("computers-choice")
 const allChoices = document.querySelectorAll('button')
 const playerImage = document.getElementById("player-image")
+const computerImage = document.getElementById("computer-image")
+const showResults = document.getElementById("result")
 let playerChoice
 let computerChoice
 /* event listener for playerChoice */
@@ -9,11 +11,12 @@ let computerChoice
 allChoices.forEach(allChoices => allChoices.addEventListener('click', (e) => {
     playerChoice = e.target.id
     playerResult.innerHTML = playerChoice
+    generateResult()
     playerImage.src = `assets/images/${[playerChoice]}.png.png`;
     generateChoiceComputer()
 }))
-/* Generate random number and assign name for computerChoice, call other functions. */
- 
+
+
 function generateChoiceComputer() {
     const randomNumber = Math.floor(Math.random() * 3) + 1
    
@@ -30,4 +33,29 @@ function generateChoiceComputer() {
     computerImage.src = `assets/images/${[computerChoice]}.png.png`;
   }
    
+  function generateResult() {
+    if (computerChoice === playerChoice) {
+      result = "It's a draw!"
+    }
+    if (computerChoice === 'rock' && playerChoice === "paper") {
+      result = "You win!"
+    }
+    if (computerChoice === "rock" && playerChoice === "scissors") {
+      result = "You lose!"
+    }
+    if (computerChoice === "paper" && playerChoice === "scissors") {
+      result = "You win!"
+    }
+    if (computerChoice === "paper" && playerChoice === "rock") {
+      result = "You lose!"
+    }
+    if (computerChoice === "scissors" && playerChoice === "rock") {
+      result = "You win!"
+    }
+    if (computerChoice === "scissors" && playerChoice === "paper") {
+      result = "you lose!"
+    }
+    showResults.innerHTML = result;
+   
+  }
   

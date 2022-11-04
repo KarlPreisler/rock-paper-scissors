5970c8d275f1698bef65ce2eb0e6bf5fa4c1f6f8
KarlPreisler <Karlpreisler@gmail.com>
2022-11-04 11:25:58 +0000

    create function for computers choice, call function in click event

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,10 +3,31 @@ const computerResult = document.getElementById("computers-choice")
 const allChoices = document.querySelectorAll('button')
 const playerImage = document.getElementById("player-image")
 let playerChoice
+let computerChoice
 /* event listener for playerChoice */
 
 allChoices.forEach(allChoices => allChoices.addEventListener('click', (e) => {
     playerChoice = e.target.id
     playerResult.innerHTML = playerChoice
     playerImage.src = `assets/images/${[playerChoice]}.png.png`;
+    generateChoiceComputer()
 }))
+/* Generate random number and assign name for computerChoice, call other functions. */
+ 
+function generateChoiceComputer() {
+    const randomNumber = Math.floor(Math.random() * 3) + 1
+   
+    if (randomNumber === 1) {
+      computerChoice = "rock"
+    }
+    if (randomNumber === 2) {
+      computerChoice = "scissors"
+    }
+    if (randomNumber === 3) {
+      computerChoice = "paper"
+    }
+    computerResult.innerHTML = computerChoice
+    computerImage.src = `assets/images/${[computerChoice]}.png.png`;
+  }
+   
+  
